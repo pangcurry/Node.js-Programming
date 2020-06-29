65153ad44c8a5e2216e10bc921f4496be149ec4d
pangcurry <magicshuffle2424@hotmail.com>
2020-06-29 11:16:27 +0900

    Add : 전역변수.js

---
+++ PART 2. Node.js 기본/CHAPTER 3 - Node.js의 전역 객체/전역변수.js
@@ -0,0 +1,12 @@
+//전역 변수
+__filename  //파일경로
+__dirname   //파일이 있는 경로
+
+//ex)
+console.log('__filename: ', __filename);
+console.log('__dirname: ',__dirname);
+
+//전역 객체
+console //콘솔 출력
+exports //모듈 다루기
+process //프로그램 다루기
