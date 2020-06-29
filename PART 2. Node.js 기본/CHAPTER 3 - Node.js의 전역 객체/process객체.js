97625d2bd43c22991b7a3374b19e53087bc60adb
pangcurry <magicshuffle2424@hotmail.com>
2020-06-29 11:20:45 +0900

    Add : process객체.js

---
+++ PART 2. Node.js 기본/CHAPTER 3 - Node.js의 전역 객체/process객체.js
@@ -0,0 +1,16 @@
+//-프로그램과 관련된 정보를 나타내는 객체이다.
+//-node.js만 있는 객체
+/*
+//객체 속성
+	argv 실행 매개변수
+	env 컴퓨터 관련 정보
+	version 버전
+	versions 종속된 프로그램 버전까지
+	arch 프로세서 아키텍쳐
+	platform 플랫폼
+
+//객체 메서드
+	exit([exitCode = 0])
+	memoryUsage() 메모리 사용 정보 객체
+	uptime() 현재 프로그램 실행 시간
+*/
